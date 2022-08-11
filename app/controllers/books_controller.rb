be2a6badac6fa2867f74c9a19f2c7aaacf4a4aeb
satomi-haruna <satomi.haruna1124@gmail.com>
2022-08-11 16:03:01 +0000

    [Update] 3-12 ルートパス設定

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,14 @@
+class BooksController < ApplicationController
+  #viewで見た目を作成=>コントローラへ処理を記述
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  root :to => 'homes#top'
+  get 'homes/top' => 'homes#top'
+  #データを追加するためのルーティング 保存機能を追加する
+  post 'books' => 'books#create'
+
+  get 'books/new'
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
